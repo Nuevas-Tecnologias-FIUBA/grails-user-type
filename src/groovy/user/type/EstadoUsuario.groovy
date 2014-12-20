76b76d14c5ace95ed54724ac0e90291328479e95
Mauro Ciancio <mauro.ciancio@cymait.com>
2014-12-20 13:04:01 -0300

    implementar equals y hashcode para estado usuario

--- src/groovy/user/type/EstadoUsuario.groovy
+++ src/groovy/user/type/EstadoUsuario.groovy
@@ -1,7 +1,23 @@
 package user.type
 
+import org.codehaus.groovy.util.HashCodeHelper
+
 abstract class EstadoUsuario {
 	abstract void chequearPuedeLoguearse(Usuario usuario)
+
+	boolean equals(Object o) {
+		if (o == null) false
+		else if (o.is(this)) true
+		else if (!(o instanceof EstadoUsuario)) false
+		// la comparación se hace exclusivamente por la clase ya que no hay atributos ni estado
+		else this.class == o.class
+	}
+
+	int hashCode() {
+		int hc = HashCodeHelper.initHash()
+		hc = HashCodeHelper.updateHash(hc, this.class)
+		hc
+	}
 }
 
 class HabilitadoEstadoUsuario extends EstadoUsuario {
