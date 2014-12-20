de48b0114a530d4b1553acb129534a3e581d4a4d
Mauro Ciancio <mauro.ciancio@cymait.com>
2014-12-20 12:19:32 -0300

    agregar entidad usuario

---
+++ grails-app/domain/user/type/Usuario.groovy
@@ -0,0 +1,32 @@
+package user.type
+
+import org.codehaus.groovy.util.HashCodeHelper
+
+class Usuario {
+
+	String nombre
+	String apellido
+
+	String nombreDeUsuario
+
+	static constraints = {
+		nombre blank: false, nullable: false
+		apellido  blank: false, nullable: false
+		nombreDeUsuario blank: false, nullable: false, unique: true
+	}
+
+	boolean equals(Object o) {
+		if (o == null) false
+		else if (o.is(this)) true
+		// usamos instanceof y no comparación de clases ya que hibernate en algunos casos crea clases
+		// on the fly para hacer proxys y en ese caso no matchearían las clases
+		else if (!(o instanceof Usuario)) false
+		else this.nombreDeUsuario == o.nombreDeUsuario
+	}
+
+	int hashCode() {
+		int hc = HashCodeHelper.initHash()
+		hc = HashCodeHelper.updateHash(hc, nombreDeUsuario)
+		hc
+	}
+}
