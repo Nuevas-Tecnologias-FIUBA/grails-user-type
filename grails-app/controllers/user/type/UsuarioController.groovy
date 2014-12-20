0b3fd0da09ff99cbcf4d32c5a20e57c9c65c3275
Mauro Ciancio <mauro.ciancio@cymait.com>
2014-12-20 13:14:05 -0300

    ejemplos de usuario en controlador

--- grails-app/controllers/user/type/UsuarioController.groovy
+++ grails-app/controllers/user/type/UsuarioController.groovy
@@ -5,4 +5,17 @@ class UsuarioController {
 	def index() {
 		render "hola!"
 	}
+
+	def crearUsuario() {
+		Usuario nuevo = Usuario.withTransaction {
+			new Usuario(params.nombreDeUsuario, params.apellido, params.nombre).save(failOnError: true)
+		}
+
+		render "Usuario nuevo creado=${nuevo}"
+	}
+
+	def buscarUsuario() {
+		Usuario usuario = Usuario.get(params.id)
+		render "Usuario encontrado=${usuario} estado=${usuario?.estado}"
+	}
 }
